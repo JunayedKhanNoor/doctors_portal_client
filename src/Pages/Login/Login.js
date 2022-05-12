f7f1189b1986057b60136f7c9998cff003b4e187
163002029.junayed <junayednoor@gmail.com>
2022-05-12 12:54:54 +0600

    React hook form

--- src/Pages/Login/Login.js
+++ src/Pages/Login/Login.js
@@ -1,11 +1,60 @@
-import React from 'react';
+import React from "react";
+import { useSignInWithGoogle } from "react-firebase-hooks/auth";
+import auth from "../../firebase.init";
+import { useForm } from "react-hook-form";
 
 const Login = () => {
-    return (
-        <div>
-            <h1>Login</h1>
+  const [signInWithGoogle, user, loading, error] = useSignInWithGoogle(auth);
+  const {
+    register,
+    formState: { errors },
+    handleSubmit,
+  } = useForm();
+  const onSubmit = (data) => {
+    console.log(data);
+  };
+  console.log(user);
+  return (
+    <div className="flex justify-center items-center h-screen">
+      <div className="card w-full md:max-w-md bg-base-100 shadow-xl">
+        <div className="card-body">
+          <h2 className="text-center text-2xl font-bold">Login</h2>
+          <form onSubmit={handleSubmit(onSubmit)}>
+            <div className="form-control w-full">
+              <label className="label">
+                <span className="label-text">Email</span>
+              </label>
+              <input
+                type="email"
+                placeholder="Your Email"
+                className="input input-bordered w-full"
+                {...register("email", {
+                    pattern: {
+                      value: /[A-Za-z]{3}/,
+                      message: 'error message'
+                    }
+                  })}
+              />
+              <label className="label">
+                <span className="label-text-alt">Alt label</span>
+              </label>
+            </div>
+            {/* <input {...register("firstName", { required: true })} />
+            {errors.firstName?.type === "required" && "First name is required"}
+
+            <input {...register("lastName", { required: true })} />
+            {errors.lastName && "Last name is required"} */}
+
+            <input type="submit" />
+          </form>
+          <div className="divider">OR</div>
+          <button onClick={() => signInWithGoogle()} className="btn btn-outline uppercase">
+            Continue with Google
+          </button>
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default Login;
