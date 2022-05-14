2a645564b451c60d40ee3e9a3706d96434948a31
163002029.junayed <junayednoor@gmail.com>
2022-05-14 22:40:18 +0600

    Dashboard

--- src/App.js
+++ src/App.js
@@ -7,8 +7,12 @@ import Login from "./Pages/Login/Login";
 import Appointment from "./Pages/Appointment/Appointment";
 import SignUp from "./Pages/Login/SignUp";
 import RequiredAuth from "./Pages/Login/RequiredAuth";
-import { ToastContainer} from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import Dashboard from "./Pages/Dashboard/Dashboard";
+import MyAppointments from "./Pages/Dashboard/MyAppointments";
+import MyReview from "./Pages/Dashboard/MyReview";
+import MyHistory from "./Pages/Dashboard/MyHistory";
 
 function App() {
   return (
@@ -24,7 +28,18 @@ function App() {
               <Appointment />
             </RequiredAuth>
           }
-        />
+        ></Route>
+        <Route
+          path="/dashboard"
+          element={
+            <RequiredAuth>
+              <Dashboard />
+            </RequiredAuth>
+          }>
+            <Route index element={<MyAppointments></MyAppointments>}></Route>
+            <Route path="review" element={<MyReview></MyReview>}></Route>
+            <Route path="history" element={<MyHistory></MyHistory>}></Route>
+          </Route>
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<SignUp />} />
       </Routes>
--- src/Pages/Shared/Navbar.js
+++ src/Pages/Shared/Navbar.js
@@ -5,18 +5,41 @@ import { Link } from "react-router-dom";
 import auth from "../../firebase.init";
 
 const Navbar = () => {
-  const [user, loading, error] = useAuthState(auth);
+  const [user] = useAuthState(auth);
   const logout = () => {
     signOut(auth);
   };
   const menuItems = (
     <>
-      <li><Link to="/">Home</Link></li>
-      <li><Link to="/about">About</Link></li>
-      <li><Link to="/appointment">Appointment</Link></li>
-      <li><Link to="/review">Reviews</Link></li>
-      <li><Link to="/contact">Contact Us</Link></li>
-      <li>{user?<button onClick={logout} className="btn btn-ghost">Sign Out</button> :<Link to="/login">Login</Link>}</li>
+      <li>
+        <Link to="/">Home</Link>
+      </li>
+      <li>
+        <Link to="/appointment">Appointment</Link>
+      </li>
+      <li>
+        <Link to="/review">Reviews</Link>
+      </li>
+      <li>
+        <Link to="/contact">Contact Us</Link>
+      </li>
+      <li>
+        <Link to="/about">About</Link>
+      </li>
+      {user && (
+        <li>
+          <Link to="/dashboard">Dashboard</Link>
+        </li>
+      )}
+      <li>
+        {user ? (
+          <button onClick={logout} className="btn btn-ghost">
+            Sign Out
+          </button>
+        ) : (
+          <Link to="/login">Login</Link>
+        )}
+      </li>
     </>
   );
   return (
@@ -50,6 +73,27 @@ const Navbar = () => {
       </div>
       <div className="navbar-center hidden lg:flex">
         <ul className="menu menu-horizontal p-0">{menuItems}</ul>
+      </div>
+      <div className="navbar-end">
+      <label tabIndex="1" htmlFor="dashboard-sidebar" className="btn btn-ghost lg:hidden">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-5 w-5"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M4 6h16M4 12h8m-8 6h16"
+              />
+            </svg>
+          </label>
+        {/* <label htmlFor="dashboard-sidebar" class="btn btn-primary drawer-button lg:hidden">
+          Open drawer
+        </label> */}
       </div>
     </div>
   );
